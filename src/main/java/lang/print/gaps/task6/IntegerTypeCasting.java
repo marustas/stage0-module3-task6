f4c51de787d30ffb786dfab6a820d3c4fc755869
Stanislau Marudau <marudov.stas@gmail.com>
2022-06-28 22:37:04 +0300

    Second try of sixth task

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,8 +3,7 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first =1000L;
-        int second=2;
-        first=second;
+        int second=1000;
         System.out.println(first+"\n"+second);
     }
 }
